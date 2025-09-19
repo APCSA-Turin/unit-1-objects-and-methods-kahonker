090045ca6ae2ebea7f675f21a8acbce35ba2e5d9
Alexey Khoroshenkiy <alexeyk4@nycstudents.net>
2025-09-19 13:00:15 -0400

    u1t5 Lab 2

--- u1t5/DecimalChange.java
+++ u1t5/DecimalChange.java
@@ -7,11 +7,11 @@ public class DecimalChange {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is your number? ");
         double originalNum = scanner.nextDouble();
-        int adjustedHundreds = (int)((originalNum+100)/100);
-        int adjustedTens = (int)(((originalNum+10)%100)/10) + 1;
-        int adjustedOnes = (int)(((originalNum+1)%10)) + 1;
-        int adjustedTenths = (int)(((originalNum+.1)%1)*10) + 1;
-        int adjustedHundredths = (int)(((originalNum+.01)%.1)*100) + 1;
+        int adjustedHundreds = (int)(((originalNum+100)%1000)/100);
+        int adjustedTens = (int)(((originalNum+10)%100)/10);
+        int adjustedOnes = (int)(((originalNum+1)%10));
+        int adjustedTenths = (int)(((originalNum+.10001)%1)*10);
+        int adjustedHundredths = (int)(((originalNum+.010001)%.1)*100);
         System.out.println("Your adjusted number is: " + adjustedHundreds + adjustedTens + adjustedOnes + "." + adjustedTenths + adjustedHundredths);
 
     }
